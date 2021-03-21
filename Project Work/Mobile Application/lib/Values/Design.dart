9375d91a02aa56c7dec5477e1ef68ecbc77d0d43
Sarthak <sarthak2580@gmail.com>
2021-03-21 19:04:45 +0530

    - added color and size constants for Design/Layout

---
+++ Project Work/Mobile Application/lib/Values/Design.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Design{
+  static final Color backgroundPrimary = Color(0xFF0A0908);
+
+  static final Color accentPrimary = Color(0xFFA9927D);
+  static final Color accentSecondary = Color(0xFF5E5035);
+
+  static final Color textPrimary = Color(0xFFABABAB);
+  static final double textSmall = 12.0;
+  static final double textMedium = 16.0;
+  static final double textLarge = 20.0;
+
+  static final double radiusSmall = 8.0;
+  static final double radiusMedium = 12.0;
+  static final double radiusLarge = 18.0;
+}
