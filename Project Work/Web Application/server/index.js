d3b5a4096b82557ee09303c5d44d24257d0a3095
ArreShreya <srjoshi533@gmail.com>
2021-03-21 18:30:57 +0530

    initial commit

---
+++ Project Work/Web Application/server/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const bodyParser = require('body-parser');
+const router = express.Router();
+const fs = require('fs');
+
+const cors = require('cors')
+const pool = require('./db')
+
+const path = require('path')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/restaurantmanager', require('./routs/resmanager'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Serer started on port ${PORT}`));
