63d512d17bbcce8a8252ad81e8cf917e3babe863
ArreShreya <srjoshi533@gmail.com>
2021-03-27 01:49:55 +0530

    fixed queires in resmanager.js

--- Project Work/Web Application/routes/resmanager.js
+++ Project Work/Web Application/routes/resmanager.js
@@ -17,7 +17,7 @@ router.post('/login', async(req,res) => {
     try {
         const data = req.body;
         pool.query("SET search_path TO 'restaurant_db';");
-        const login = await pool.query("SELECT user_id, password FROM restaurant_db.manager WHERE user_id=$1 and password=$2", [data.user_id, data.password]);
+        const login = await pool.query("SELECT user_id, password FROM manager WHERE user_id=$1 and password=$2", [data.user_id, data.password]);
 
         console.log("login data res", login.rows);
 
@@ -39,7 +39,7 @@ router.post('/login', async(req,res) => {
 router.post('/viewmenu', async(req, res) => {
     try {
         const data = req.body;
-        const viewmenu = await pool.query("SELECT dish_id, dish_name, description, dish_price, status, jain_availability FROM restaurant_db.menu WHERE restaurant_id=$1", [data.restaurant_id]);
+        const viewmenu = await pool.query("SELECT dish_id, dish_name, description, dish_price, status, jain_availability FROM menu WHERE restaurant_id=$1", [data.restaurant_id]);
 
         if(!viewmenu.rows[0] && !viewmenu.rows.length){
             res.status(400).json({
@@ -63,7 +63,7 @@ router.post('/revenue', async(req, res) => {
     try {
         const data = req.body;
 
-        const final = await pool.query("SELECT SUM(final_bill) FROM restaurant_db.revenue WHERE time_stamp>=$1 and time_stamp<=$2", [data.from_time_stamp
+        const final = await pool.query("SELECT SUM(final_bill) FROM revenue WHERE time_stamp>=$1 and time_stamp<=$2", [data.from_time_stamp
         , data.to_time_stamp]);
 
         if(!final.rows[0] && !final.rows.length){
@@ -95,10 +95,10 @@ router.post('/register_user',async(req,res) => {
 
         } else { 
 
-            const usertypeid = await pool.query("SELECT usertype_id from restaurant_db.usertype WHERE user_role=$1", [data.user_role])
+            const usertypeid = await pool.query("SELECT usertype_id from usertype WHERE user_role=$1", [data.user_role])
             const usertypeid2 = usertypeid.rows[0].usertype_id;
                 const newUser = await pool.query(
-                "INSERT INTO restaurant_db.users(restaurant_id,user_image,usertype_id,user_name,email_id,contact_no,contact_no_optional,password) VALUES ($1, $2, $3,$4, $5,$6,$7,$8)",
+                "INSERT INTO users(restaurant_id,user_image,usertype_id,user_name,email_id,contact_no,contact_no_optional,password) VALUES ($1, $2, $3,$4, $5,$6,$7,$8)",
                 [data.restaurant_id, data.user_image, usertypeid2, data.user_name, data.email_id, data.contact_no, data.contact_no_optional, data.password]);
                
                 res.json({msg:"User added" });
@@ -125,10 +125,10 @@ router.post('/mark_attendance',async(req,res) => {
            
             const time_now= await pool.query("SELECT NOW()");
             
-            const username = await pool.query("SELECT user_name from restaurant_db.users WHERE user_id=$1", [data.user_id])
+            const username = await pool.query("SELECT user_name from users WHERE user_id=$1", [data.user_id])
             const username2 = username.rows[0].user_name;
             const newUser = await pool.query(
-            "INSERT INTO restaurant_db.attendance(restaurant_id,user_id, user_name, time_stamp,attendance_status) VALUES ($1, $2, $3, $4, $5)",
+            "INSERT INTO attendance(restaurant_id,user_id, user_name, time_stamp,attendance_status) VALUES ($1, $2, $3, $4, $5)",
                 [data.restaurant_id,data.user_id, username2, time_now.rows[0]['now'],data.attendance_status]);
                 
             res.json({msg:"Attendance marked" });
@@ -143,7 +143,7 @@ router.post('/mark_attendance',async(req,res) => {
 // ATTENDANCE
 router.get('/view_attendance', async (req,res)=>{
     try {
-        const results = await pool.query('SELECT user_id,user_name,time_stamp,attendance_status FROM restaurant_db.ATTENDANCE');
+        const results = await pool.query('SELECT user_id,user_name,time_stamp,attendance_status FROM ATTENDANCE');
         //console.log(results.rows[0]);
         if(!results.rows[0] && !results.rows.length)
         {
@@ -174,7 +174,7 @@ router.post('/view_attendance',async (req,res)=>{
             }); 
         }
         else{
-            const results = await pool.query(`SELECT user_id,user_name,time_stamp,attendance_status FROM restaurant_db.ATTENDANCE where user_name like '%${req.body.name}%'`)
+            const results = await pool.query(`SELECT user_id,user_name,time_stamp,attendance_status FROM ATTENDANCE where user_name like '%${req.body.name}%'`)
             //console.log(results)
             if(!results.rows[0] && !results.rows.length)
             {
@@ -197,7 +197,7 @@ router.post('/view_attendance',async (req,res)=>{
 // FEEDBACK
 router.get('/feedback',async (req,res)=>{
     try{
-        const results = await pool.query(`select FEEDBACK_ID,CATEGORY1,CATEGORY2,CATEGORY3,CATEGORY4 from restaurant_db.feedback`);
+        const results = await pool.query(`select FEEDBACK_ID,CATEGORY1,CATEGORY2,CATEGORY3,CATEGORY4 from feedback`);
         if(!results.rows[0] && !results.rows.length)
         {
             res.status(400).json({
@@ -227,7 +227,7 @@ router.post('/feedback',async (req,res)=>{
             }); 
         }
         else {
-           const results = await pool.query("select FEEDBACK_ID,CATEGORY1,CATEGORY2,CATEGORY3,CATEGORY4 from restaurant_db.feedback where CATEGORY1 = $1 or CATEGORY2 = $1 or CATEGORY3 = $1 or CATEGORY4 = $1", [query_detail]);
+           const results = await pool.query("select FEEDBACK_ID,CATEGORY1,CATEGORY2,CATEGORY3,CATEGORY4 from feedback where CATEGORY1 = $1 or CATEGORY2 = $1 or CATEGORY3 = $1 or CATEGORY4 = $1", [query_detail]);
 
             if(!results.rows[0] && !results.rows.length)
             {
