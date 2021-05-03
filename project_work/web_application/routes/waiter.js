55e36eba11016ae3d003689c87f96de601800f88
Pratvi27 <67515005+Pratvi27@users.noreply.github.com>
2021-05-03 22:48:13 +0530

    Modified login, added viewmenu endpoint and initial commit of fcm

--- project_work/web_application/routes/waiter.js
+++ project_work/web_application/routes/waiter.js
@@ -11,6 +11,18 @@ const jwt=require('jsonwebtoken')
 const cors = require('cors')
 const pool = require('../db')
 
+
+var admin = require('firebase');
+var serviceAccount = require("routes\canteen-management-456ca-firebase-adminsdk-9j8r5-1317415eb1.json");
+var app_fcm = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+
+
+
+
+
+// login + jwt + email
 router.post('/login', async(req,res) => {
     try {
         const data = req.body;
@@ -23,84 +35,110 @@ router.post('/login', async(req,res) => {
                 error: 1,
                 msg: "A user with this email ID does not exist!"
             });
-        }
-
-        else{
+        }else{
             // validate password
-                bcrypt.compare(data.password, login.rows[0].password, async function(err, result1){
-                    console.log(result1)
-                    // handle bcrypt compare error
-                    if (result1==false) { 
-                        return res.status(400).json({
-                            error:1,
-                            msg: "Invalid password! Please try again!"
-                        });
-                        throw (err); 
+            bcrypt.compare(data.password, login.rows[0].password, async function(err, result2) {
+                
+                // handle bcrypt compare error
+                if (err) { 
+                    throw (err); 
+                }
+                
+                //GET JWT TOKEN 
+                const emailid=data.email_id
+                jwt.sign({emailid},'secretkey',{expiresIn: '5h'},async (err,token)=>{
+                    
+                    if(err){
+                        console.log(err.message)
                     }else{
                         
-                            const result = await pool.query("SELECT * FROM users WHERE email_id=$1", [data.email_id]);
-                            // password matches
-                            // console.log(result.rows[0]['password'])
-                            // if(result.rows[0]['password'] === req.body.password){
-
-                            //GET JWT TOKEN 
-                            const emailid=data.email_id
-                            
-                            jwt.sign({emailid},'secretkey',{expiresIn: '5h'},async (err,token)=>{
+                        const fcmToken="temporary_fcm_token"   
+                        const checkEntry =  await pool.query("SELECT email_id FROM fcm_jwt WHERE email_id=$1", [data.email_id]);
+                        
+                        //if already logged in then update
+                        if(checkEntry && typeof(checkEntry.rows[0])!=='undefined')
+                        {
+                            console.log('updating')
+                            const newUser = await pool.query(
+                                "UPDATE fcm_jwt SET last_jwt = $1 WHERE email_id=$2", [token, data.email_id]);
                                 
-                                if(err){
-                                    console.log(err.message)
-                                }else{
-                                    
-                                    //need to create fcm token dont know how
-                                    const fcmToken="temporary_fcm_token"   
-                                    const checkEntry =  await pool.query("SELECT email_id FROM fcm_jwt WHERE email_id=$1", [data.email_id]);
-                                    
-                                    // //if already logged in then update
-                                    if(checkEntry && typeof(checkEntry.rows[0])!=='undefined')
-                                    {
-                                        console.log('updating')
-                                        const newUser = await pool.query(
-                                            "UPDATE fcm_jwt SET last_jwt = $1 WHERE email_id=$2", [token, data.email_id]);
-                                            
-                                    }//if there is no login info then insert
-                                    else{
-                                            console.log('inserting')
-                                            const newUser = await pool.query(
-                                            "INSERT INTO fcm_jwt(email_id,fcm_token,last_jwt) VALUES ($1, $2, $3)",
-                                            [data.email_id, fcmToken, token]);
-                                            
-                                    }
+                        }//if there is no login info then insert
+                        else{
+                                console.log('inserting')
+                                const newUser = await pool.query(
+                                "INSERT INTO fcm_jwt(email_id,fcm_token,last_jwt) VALUES ($1, $2, $3)",
+                                [data.email_id, fcmToken, token]);
+                                
+                        }
 
-                                    //TOKEN CREATED WITHOUT ERROR  RETURN IT ALONG WITH LOGIN DATA
-                                    return  res.status(200).json({
-                                        token: token,
-                                        msg: "Successfully logged in!",
-                                        user_id: login.rows[0].user_id,
-                                        restaurant_id: login.rows[0].restaurant_id,
-                                        usertype_id: login.rows[0].usertype_id,
-                                        user_name: login.rows[0].user_name,
-                                        contact_no: login.rows[0].contact_no,
-                                        user_image: login.rows[0].user_image
-                                    });
-                                }
-                            
-                            
-                             }); 
-                    }// invalid password
-                    // else {
-                    //     return res.status(400).json({
-                    //         error:1,
-                    //         msg: "Invalid password! Please try again!"
-                    //     });
-                    // }
-                });
-                         
-            }
-        
-        } catch (err) {
-            console.log(err.message);
+                        //TOKEN CREATED WITHOUT ERROR  RETURN IT ALONG WITH LOGIN DATA
+                        return  res.status(200).json({
+                                // token: token,
+                                msg: "Successfully logged in!",
+                                user_id: login.rows[0].user_id,
+                                user_name: login.rows[0].user_name,
+                        });
+                    }
+                    
+                }); 
+                
+            });
+            
+                    
+                 
         }
+        
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
+
+// save_fcm_token : fcm, jwt
+router.post('/save_fcm_token',verifyToken, async(req,res) => {
+
+    const data = req.body;
+    const email =  await pool.query("SELECT email_id from fcm_jwt WHERE last_jwt=$1", [data.jwt]);
+
+    if(!email.rows[0] && !email.rows.length){
+        res.status(400).json({msg: "Invalid jwt-token!"})        
+    }
+    else{
+        const newUser = await pool.query(
+            "UPDATE fcm_jwt SET fcm_token = $1 WHERE last_jwt=$2", [data.token, data.jwt]);
+        return res.json({msg: "FCM token updated."});
+    }   
+})
+
+
+router.post('/viewmenu',verifyToken, async(req, res) => {
+    
+    //INITIALIZE JWT VERIFICATION
+    jwt.verify(req.token, 'secretkey',async (err,authData)=>{
+        if(err){
+            console.log(err)
+            //INVALID TOKEN/TIMEOUT so delete the entry from databse 
+            // const deleted_info = await pool.query("DELETE FROM fcm_jwt WHERE EMAIL_ID=$1 ",[req.body.email_id])
+            res.status(400).json({msg: "Session expired. Login again"})
+            
+        }else{
+            //WHEN USER HAS VALID JWT TOKEN
+            const data = req.body;
+            const viewmenu =  await pool.query("SELECT dish_id, dish_name, description, dish_price, status, jain_availability FROM menu WHERE restaurant_id=$1", [data.restaurant_id]);
+            
+            if(!viewmenu.rows[0] && !viewmenu.rows.length){
+                return res.status(400).json({
+                    error:1,
+                    msg: "No menu item available for this restaurant"
+                });
+              
+            }
+            else{
+                return res.json({total_results: viewmenu.rowCount, dishes: viewmenu.rows});
+            }    
+        }
+    });   
+    
 })
 
 
